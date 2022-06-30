7d1d99a1c505d80ded643cace255849321d35c7f
pseudozach <git@pseudozach.com>
2022-06-30 00:22:03 -0700

    add swap api update UI

--- lnbits/extensions/swap/views_api.py
+++ lnbits/extensions/swap/views_api.py
@@ -1,35 +1,131 @@
-# views_api.py is for you API endpoints that could be hit by another service
+from datetime import datetime
+from http import HTTPStatus
 
-# add your dependencies here
+from fastapi.param_functions import Body
+from fastapi.params import Depends, Query
+from starlette.exceptions import HTTPException
+from starlette.requests import Request
 
-# import httpx
-# (use httpx just like requests, except instead of response.ok there's only the
-#  response.is_error that is its inverse)
+from lnbits.core.crud import get_user
+from lnbits.core.services import perform_lnurlauth, redeem_lnurl_withdraw
+from lnbits.decorators import WalletTypeInfo, get_key_type, require_admin_key
+
+from .models import (
+    SubmarineSwap,
+    ReverseSubmarineSwap,
+    CreateSubmarineSwap,
+    CreateReverseSubmarineSwap,
+)
 
 from . import swap_ext
 
-# add your endpoints here
+from .swap import (
+    create_refund_tx,
+    get_boltz_pairs,
+    get_swap_status,
+    MEMPOOL_SPACE_URL,
+    BOLTZ_URL,
+)
+
+from .crud import (
+    update_swap_status,
+    create_submarine_swap,
+    get_submarine_swaps,
+    get_submarine_swap,
+    create_reverse_submarine_swap,
+    get_reverse_submarine_swaps,
+    get_reverse_submarine_swap,
+)
+
+@swap_ext.get("/api/v1/mempool-url")
+async def api_mempool_url():
+    return MEMPOOL_SPACE_URL
+
+@swap_ext.get("/api/v1/boltz-url")
+async def api_boltz_url():
+    return BOLTZ_URL
+
+@swap_ext.get('/api/v1/gethexbuffer/{hexstring}')
+async def api_gethexbuffer(hexstring: str):
+    return bytearray.fromhex(hexstring)
+
+# NORMAL SWAP
+@swap_ext.get("/api/v1/submarineswap")
+async def api_submarineswap(
+    g: WalletTypeInfo = Depends(get_key_type),
+    all_wallets: bool = Query(False),
+):
+    wallet_ids = [g.wallet.id]
+    if all_wallets:
+        wallet_ids = (await get_user(g.wallet.user)).wallet_ids
+    return [swap.dict() for swap in await get_submarine_swaps(wallet_ids)]
+
+@swap_ext.get("/api/v1/submarineswap-refund/{submarineSwapId}")
+async def api_submarineswap_refund(submarineSwapId: str):
+    swap = await get_submarine_swap(submarineSwapId)
+    if swap == None:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail="submarine swap does not exist."
+        )
+    await create_refund_tx(swap)
+    await update_swap_status(swap, "refunded")
+    return swap
+
+@swap_ext.post("/api/v1/submarineswap")
+async def api_submarineswap_create(
+    data: CreateSubmarineSwap,
+    wallet: WalletTypeInfo = Depends(require_admin_key)
+):
+    swap = await create_submarine_swap(data)
+    return swap.dict()
+
+# REVERSE SWAP
+@swap_ext.get("/api/v1/reverse-submarineswap")
+async def api_reverse_submarineswap(
+    g: WalletTypeInfo = Depends(get_key_type),
+    all_wallets: bool = Query(False),
+):
+    wallet_ids = [g.wallet.id]
+    if all_wallets:
+        wallet_ids = (await get_user(g.wallet.user)).wallet_ids
+    return [swap.dict() for swap in await get_reverse_submarine_swaps(wallet_ids)]
+
+@swap_ext.post("/api/v1/reverse-submarineswap")
+async def api_reverse_submarineswap_create(
+    data: CreateReverseSubmarineSwap,
+    wallet: WalletTypeInfo = Depends(require_admin_key)
+):
+    swap = await create_reverse_submarine_swap(data)
+    return swap.dict()
+
+# STATUS
+@swap_ext.get("/api/v1/swap-status/{swap_id}")
+async def api_submarineswap_status(swap_id: str):
+    swap = await get_submarine_swap(swap_id)
+    if swap == None:
+        swap = await get_reverse_submarine_swap(swap_id)
+        if swap == None:
+            raise HTTPException(
+                status_code=HTTPStatus.NOT_FOUND, detail="swap does not exist."
+            )
+    return get_swap_status(swap)
 
 
-@swap_ext.get("/api/v1/tools")
-async def api_swap():
-    """Try to add descriptions for others."""
-    tools = [
-        {
-            "name": "fastAPI",
-            "url": "https://fastapi.tiangolo.com/",
-            "language": "Python",
-        },
-        {
-            "name": "Vue.js",
-            "url": "https://vuejs.org/",
-            "language": "JavaScript",
-        },
-        {
-            "name": "Quasar Framework",
-            "url": "https://quasar.dev/",
-            "language": "JavaScript",
-        },
-    ]
+@swap_ext.get("/api/v1/check-swaps")
+async def api_check_swaps(
+    g: WalletTypeInfo = Depends(get_key_type),
+    all_wallets: bool = Query(False),
+):
+    wallet_ids = [g.wallet.id]
+    if all_wallets:
+        wallet_ids = (await get_user(g.wallet.user)).wallet_ids
+    status = []
+    for swap in await get_submarine_swaps(wallet_ids):
+        if swap.status == "pending":
+            status.append(get_swap_status(swap))
+    return status
 
-    return tools
+@swap_ext.get("/api/v1/boltz-config")
+async def api_boltz_config():
+    res = get_boltz_pairs()
+    return res["pairs"]["BTC/BTC"]
