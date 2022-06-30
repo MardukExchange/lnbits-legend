57118ad1fc7bf94a0150fd5536525be4d844c660
pseudozach <git@pseudozach.com>
2022-06-30 00:19:39 -0700

    add static files

--- lnbits/extensions/swap/__init__.py
+++ lnbits/extensions/swap/__init__.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from fastapi.staticfiles import StaticFiles
 
 from lnbits.db import Database
 from lnbits.helpers import template_renderer
@@ -7,6 +8,13 @@ db = Database("ext_swap")
 
 swap_ext: APIRouter = APIRouter(prefix="/swap", tags=["swap"])
 
+swap_static_files = [
+    {
+        "path": "/swap/static",
+        "app": StaticFiles(directory="lnbits/extensions/swap/static"),
+        "name": "swap_static",
+    }
+]
 
 def swap_renderer():
     return template_renderer(["lnbits/extensions/swap/templates"])
